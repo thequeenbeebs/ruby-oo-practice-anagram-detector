bad4e4b4729fa216fdf96777a809f6ad0f392864
Blaire Baker <bakerblaire@gmail.com>
2020-11-21 14:31:07 -0600

    commit tree dc641addf58082fe9f64ecf7ecbbe931d3fc3692

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+    attr_accessor :string
+
+    def initialize(string)
+        @string = string.split("")
+    end
+
+    def match(array)
+        anagrams = []
+        self.string = string.sort
+        array.each do |word|
+            anagram = word.split("").sort
+            if anagram == self.string
+                anagrams << word
+            end
+        end
+        anagrams
+    end
+
+end
